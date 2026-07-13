d2709614838b8dc4fd1ceda09822b39ad19f56a9
Montibus <38812680+Montibus@users.noreply.github.com>
2026-07-13 20:30:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
 self.addEventListener('fetch', function(event) {
-  // Required by Chrome to trigger the PWA Install Prompt
+  // Required by Chrome to trigger the PWA Install Prompt.
+  // We leave it empty as we do not need offline caching for this app.
 });
